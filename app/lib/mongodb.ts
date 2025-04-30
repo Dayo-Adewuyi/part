3d41860d760c3e605c980152a6d80e620fd98d6f
dayo-adewuyi <dayoadewuyi@protonmail.com>
2025-04-30 16:58:53 +0100

    fix

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -13,22 +13,11 @@ const options = {
   },
 };
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-  let globalWithMongo = global as typeof global & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
 
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+ const client = new MongoClient(uri, options);
+ const  clientPromise = client.connect();
+
+
 
 export default clientPromise;
